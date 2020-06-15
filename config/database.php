16e1344e297defd02fde863c0e726b723bd90982
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2020-06-15 10:26:42 +0700

    vanh push code

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => 'pgsql',//env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+           // 'url' => env('DATABASE_URL'),
+            'host' => 'ec2-52-44-55-63.compute-1.amazonaws.com',//env('DB_HOST', '127.0.0.1'),
+            'port' => '5432',//env('DB_PORT', '5432'),
+            'database' => 'dc8jbunp3tqdvr',//env('DB_DATABASE', 'forge'),
+            'username' => 'pzsuszijtifpzb',//env('DB_USERNAME', 'forge'),
+            'password' => '302aca6a516fc2090cf7b9cc33ad03ea89c4a35f77e5417001690fa16d2aacd8',//env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
