03a590b6ffc1d8c8e8f3d7d593c472654ea48917
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2020-06-24 08:11:38 +0700

    vanhvanh:

--- routes/user.php
+++ routes/user.php
@@ -3,9 +3,9 @@ Route::get('/', "AbcController@index");
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get("/category/{category:slug}","HomeController@category");
-
+Route::get("/category","HomeController@category");
 Route::get("/product/{product:slug}","HomeController@product");
-
+Route::get("/product","HomeController@product");
 Route::post("/cart/add/{product}","HomeController@addToCart");
 
 Route::get("/shopping-cart","HomeController@shoppingCart");
